63bc8c2de5f9fe8a88a1a324defcd268b2ed37f8
Eddyzera <nescau23041996@gmail.com>
2021-03-15 18:11:04 -0300

    config o projeto

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home () {
+  return (
+    <div>
+      <Head>
+        <title>Eu sou Edgar</title>
+      </Head>
+
+      <h2>Hello World</h2>
+    </div>
+  )
+}
